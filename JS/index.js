5dd7d0abe7868375efdacbc2c29615134713c3bc
deyaniket10 <deyaniket10@gmail.com>
2025-07-03 11:26:14 +0530

    Banner-uodate

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.checkout-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
