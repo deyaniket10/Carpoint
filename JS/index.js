7f93a62521df7026eb8a0482d54c750b70a67699
deyaniket10 <deyaniket10@gmail.com>
2025-07-04 03:33:45 +0530

    update banner

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,14 @@
-const swiper = new Swiper('.checkout-swiper', {
+const swiper = new Swiper(".checkout-swiper", {
   slidesPerView: 3,
-  spaceBetween: 24,
+  spaceBetween: 70,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    320: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
+    320: { slidesPerView: 1, spaceBetween: 20 },
+    575: { slidesPerView: 2, spaceBetween: 40 },
     1024: { slidesPerView: 3 },
   },
 });
