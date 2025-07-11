77b2a05872522214bb12716144f8643e79f228ad
deyaniket10 <deyaniket>
2025-07-12 02:41:38 +0530

    update pages

--- JS/booking.js
+++ JS/booking.js
@@ -1,3 +1,22 @@
 // Header Section
-const nav = document.querySelector(.nav - collapse)
+const menu = document.querySelector(".ham")
+console.log(menu)
+const nav = document.querySelector(".nav-collapse")
 console.log(nav)
+menu.addEventListener("click", () => {
+    console.log("event triggered");
+    nav.classList.toggle("active");
+
+
+})
+
+// right box of cart section
+function updatePrice(value) {
+    document.getElementById('priceOutput').innerText = '₹' + value;
+}
+
+function clearAll() {
+    document.querySelectorAll('.form-check-input').forEach(el => el.checked = false);
+    document.getElementById('priceRange').value = 1000;
+    updatePrice(1000);
+}
--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,14 @@
 const swiper = new Swiper(".checkout-swiper", {
   slidesPerView: 3,
-  spaceBetween: 70,
+  spaceBetween: 40,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    320: { slidesPerView: 1, spaceBetween: 20 },
-    575: { slidesPerView: 2, spaceBetween: 40 },
+    320: { slidesPerView: 1, spaceBetween: 10 },
+    575: { slidesPerView: 2, spaceBetween: 25 },
     1024: { slidesPerView: 3 },
   },
 });
